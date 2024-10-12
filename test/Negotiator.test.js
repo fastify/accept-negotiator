f488e14ad6a004af4b5bda027d5e9d5b4a122187
dan.castillo <dan.castillo@jasper.ai>
2024-10-12 10:54:49 -0400

    add @matteo.collina/tspl for node 18 support

--- test/Negotiator.test.js
+++ test/Negotiator.test.js
@@ -1,26 +1,28 @@
 'use strict'
 
 const { test } = require('node:test')
+const { tspl } = require('@matteo.collina/tspl')
 const testCases = require('./testCases')
 const Negotiator = require('../index').Negotiator
 
 test('should create Negotiator-Instances', t => {
-  t.plan(2)
-  t.assert.ok(new Negotiator() instanceof Negotiator)
-  t.assert.ok(Negotiator() instanceof Negotiator)
+  const { ok } = tspl(t, { plan: 2 })
+
+  ok(new Negotiator() instanceof Negotiator)
+  ok(Negotiator() instanceof Negotiator)
 })
 
 for (const [header, supportedEncodings, expected] of testCases) {
   test(`should return ${expected} when ${header} and ${supportedEncodings}`, t => {
-    t.plan(4)
+    const { strictEqual } = tspl(t, { plan: 4 })
     const negotiatorNoCache = new Negotiator({ supportedValues: supportedEncodings })
-    t.assert.strictEqual(negotiatorNoCache.negotiate(header), expected, `should return ${expected} when ${header} and ${supportedEncodings}`)
+    strictEqual(negotiatorNoCache.negotiate(header), expected, `should return ${expected} when ${header} and ${supportedEncodings}`)
 
     const negotiatorNonClass = Negotiator({ supportedValues: supportedEncodings })
-    t.assert.strictEqual(negotiatorNonClass.negotiate(header), expected, `should return ${expected} when ${header} and ${supportedEncodings}`)
+    strictEqual(negotiatorNonClass.negotiate(header), expected, `should return ${expected} when ${header} and ${supportedEncodings}`)
 
     const negotiatorCache = new Negotiator({ supportedValues: supportedEncodings, cache: new Map() })
-    t.assert.strictEqual(negotiatorCache.negotiate(header), expected, `should return ${expected} when ${header} and ${supportedEncodings}`)
-    t.assert.strictEqual(negotiatorCache.negotiate(header), expected, `should return ${expected} when ${header} and ${supportedEncodings}`)
+    strictEqual(negotiatorCache.negotiate(header), expected, `should return ${expected} when ${header} and ${supportedEncodings}`)
+    strictEqual(negotiatorCache.negotiate(header), expected, `should return ${expected} when ${header} and ${supportedEncodings}`)
   })
 }
--- test/negotiate.test.js
+++ test/negotiate.test.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const { test } = require('node:test')
+const { tspl } = require('@matteo.collina/tspl')
 const testCases = require('./testCases')
 const negotiate = require('../index').negotiate
 
 for (const [header, supportedEncodings, expected] of testCases) {
   test(`should return ${expected} when ${header} and ${supportedEncodings}`, t => {
-    t.plan(1)
-    t.assert.strictEqual(negotiate(header, supportedEncodings), expected)
+    const { strictEqual } = tspl(t, { plan: 1 })
+    new Promise((resolve) => {
+      strictEqual(negotiate(header, supportedEncodings), expected)
+      resolve()
+    })
   })
 }
